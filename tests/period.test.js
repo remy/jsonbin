48081bf83edff244903b76c25ab0b48173fc95a5
Remy Sharp <remy@remysharp.com>
2017-02-07 17:05:05 +0000

    test: for period requests

---
+++ tests/period.test.js
@@ -0,0 +1,40 @@
+const tap = require('tap');
+const test = require('tap-only');
+const { setup, teardown, request, base, _request } = require('./utils');
+let user = null;
+
+test('load user', t => {
+  return setup({
+    urls: {}
+  }).then(u => user = u).then(() => t.pass('user loaded'));
+});
+
+// reset the user store
+tap.afterEach(done => {
+  user.store = {
+    urls: {}
+  };
+  user.dirty();
+  user.save().then(() => done());
+});
+
+tap.tearDown(() => {
+  return teardown();
+});
+
+test('POST url["one.two"]', t => {
+  return request(user, {
+    method: 'POST',
+    body: {
+      'one.two': true
+    },
+  }).then(body => {
+    t.deepEqual(body, { 'one.two': true }, 'body matches');
+
+    return request(user, {
+      url: base + '/one.two'
+    }).then(body => {
+      t.equal(body, true, `${base}/one.two matches ${body}`)
+    });
+  });
+});
