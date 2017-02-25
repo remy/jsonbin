54602f6c150236aaaa68cc9ed64cbedbead8a273
Remy Sharp <remy@remysharp.com>
2017-02-25 16:04:05 +0000

    test: automate issue tests

---
+++ tests/operations.test.js
@@ -0,0 +1,82 @@
+const tap = require('tap');
+const test = require('tap-only');
+const fs = require('fs');
+const { op, setup, teardown, updateUser, _request: request } = require('./utils');
+let user = null;
+
+test('load user', t => {
+  return setup({}).then(u => user = u).then(() => t.pass('user loaded'));
+});
+
+// reset the user store
+tap.afterEach(done => {
+  user.store = {};
+  user.dirty();
+  user.save().then(() => done());
+});
+
+tap.tearDown(() => {
+  return teardown();
+});
+
+const dir = __dirname + '/operations/';
+const tests = fs.readdirSync(dir).reduce((acc, file) => {
+  const [ id, place ] = file.split('.');
+  if (!acc[id]) {
+    acc[id] = {};
+  }
+
+  if (place === 'op') {
+    acc[id][place] = op(dir + file);
+  } else {
+    acc[id][place] = require(dir + file);
+  }
+
+  return acc;
+}, {});
+
+Object.keys(tests).forEach(id => {
+  test(`issue #${id}`, t => {
+    // setup
+    user.store = tests[id].start;
+    const expect = tests[id].expect;
+    user.dirty();
+    return user.save().then(() => {
+      const tokens = {
+        token: user.apikey,
+        bearer: user.generateBearer(),
+      }
+
+      const requests = tests[id].op.reduce((acc, op) => {
+        if (op.headers.authorization) {
+          const [ scheme, value ] = op.headers.authorization.split(' ');
+          op.headers.authorization = scheme + ' ' + tokens[scheme];
+        }
+
+        return acc.then(() => request({
+          url: op.url,
+          headers: op.headers,
+          method: op.method,
+          body: op.body
+        }).then(res => {
+          t.ok(res.statusCode < 300, `Complete ${op.method} ${op.url}: ${res.statusCode}`);
+        }))
+      }, Promise.resolve());
+
+      return requests.then(() => updateUser(user)).then(user => {
+        const { store } = user.toObject();
+        t.deepEqual(expect, store);
+      });
+    });
+  });
+})
+
+
+
+
+
+
+
+
+
+
--- tests/utils.js
+++ tests/utils.js
@@ -5,17 +5,21 @@ dotenv.config({
 });
 
 const mongoose = require('../lib/db/');
+const fs = require('fs');
 const User = require('../lib/db/user');
 const _request = require('./request-then');
 // "test" is the username
-const base = `http://localhost:${process.env.PORT}/test`;
+const root = `http://localhost:${process.env.PORT}`;
+const base = `${root}/test`;
 
 module.exports = {
   setup,
+  updateUser,
   teardown,
   request,
   base,
   _request,
+  op,
 };
 
 function request(user, {
@@ -46,8 +50,57 @@ function setup(store = {}) {
   })
 }
 
+function updateUser({ publicId }) {
+  return User.findOne({ publicId });
+}
+
 function teardown() {
   return User.remove({ username: 'test'}).then(() => {
     mongoose.connection.close();
   });
+}
+
+function op(file) {
+  const body = fs.readFileSync(file, 'utf8');
+  const requests = [];
+  let index = -1;
+  const lines = body.split('\n');
+
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i].trim();
+
+    if (line === '') {
+      continue;
+    }
+
+    if (line.startsWith('//')) { // comment
+      continue;
+    }
+
+    if (line.startsWith('# ')) {
+      // TODO check last request is done
+      let [ , method, url ] = line.split(' ');
+      index++;
+      requests[index] = {
+        method,
+        url: root + url,
+        headers: {},
+      }
+      continue;
+    }
+
+    if (line.toLowerCase() === 'headers:') {
+      i++;
+      while (line = lines[i].trim()) {
+        const parts = line.split(':');
+        requests[index].headers[parts[0].trim().toLowerCase()] = parts.slice(1).join(':').trim();
+        i++;
+      }
+      continue;
+    }
+
+    requests[index].body = line;
+  }
+
+  return requests;
 }
